f728c927c6de7232a557641eb15b5c294f148866
adenin1128 <sdt2595sc0003@edu.sca.ac.jp>
2025-10-15 15:37:52 +0900

    げーむおば

---
+++ ひな形/project/Gameover.h
@@ -0,0 +1,12 @@
+#pragma once
+#include"../Library/GameObject.h"
+
+class GameOver : public GameObject {
+public:
+	GameOver();
+	~GameOver();
+	void Update() override;
+	void Draw() override;
+private:
+	int hImage;
+};
