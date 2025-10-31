2923dc8ea9c14f9470c67f83274cb5e5f5ba230b
Rai <>
2025-10-31 17:00:11 +0900

    BOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOM!!!!!!!!!!!!!!!!!!!

--- ひな形/project/Source/Player.cpp
+++ ひな形/project/Source/Player.cpp
@@ -5,14 +5,21 @@
 #include "Gameover.h"
 #include "trap.h"
 
+
 static const float Gravity = 0.4;
 static const float v0 = -10.0;
 
+const float DRAG_COEFFICIENT = 0.05f;  // 速度の減衰係数
+const float ROT_SPEED = 0.0f;          // 回転速度
+const int SAFE_MARGIN = 100;           // 画面外判定マージン
 
-Player::Player(float startX, float startY) 
+
+
+Player::Player(float startX, float startY)
 	: x(startX), y(startY), velocity(0.0f), onGround(false)
-{   
+{
 	animImage = LoadGraph("data/image/おまえ歩き.png");
+	BOOMImage = LoadGraph("data/image/BOOM!!.png");
 	posX = 100;
 	posY = 100;
 	jumpcount = 0;
@@ -21,7 +28,11 @@ Player::Player(float startX, float startY)
 	direction = true;
 	int moveX = 1;
 	animIndex = 0;       // 最初のコマからスタートさせるやつ
-	animFrame = 0;       // アニメーションカウンターをリセットしたやつ
+	animFrame = 0; // アニメーションカウンターをリセットしたやつ
+	state = STATE_NORMAL;
+	boomAnimIndex = 0;
+	boomFrame = 0;
+
 }
 
 //コンストラクター
@@ -36,119 +47,158 @@ Player::~Player()
 //計算するところ
 void Player::Update()
 {
-	int moveX = 0;
+	if (state == STATE_NORMAL) {
+		int moveX = 0;
 
-	if (PlayerHP == 0) {
-		new GameOver();
-	}
-	if (CheckHitKey(KEY_INPUT_R))
-	{
-		PlayerHP = 0;
-	}
-	if (onGround == true) {
-		if(jumpcount < Maxjumpcount) {
-			jumpcount += 1;
+		if (PlayerHP == 0) {
+			new GameOver();
 		}
-	}
-
-	if (CheckHitKey(KEY_INPUT_D))
-	{
-		x += 3.0f; // 右に進む 
-		direction = false;
-		moveX += 2.0f;
-		Field* field = FindGameObject<Field>();
-		int push1 = field->HitCheckRight(x + 50, y + 5);
-		int push2 = field->HitCheckRight(x + 50, y + 61);
-		x -= max(push1, push2);
-		if (field->Istrap(x + 32, y + 32)) {
-			/*new Clear();*/
+		if (CheckHitKey(KEY_INPUT_R))
+		{
+			PlayerHP = 0;
 		}
-	}
-
-	if (CheckHitKey(KEY_INPUT_A)) {
-		x -= 3.0f;
-		direction = true;
-		moveX -= 2.0f;
-		Field* field = FindGameObject<Field>();
-		int push1 = field->HitCheckLeft(x + 14, y + 5);
-		int push2 = field->HitCheckLeft(x + 14, y + 61);
-		x -= max(push1, push2);
-		if (field->Istrap(x + 32, y + 32)) {
-			/*new Clear();*/
-		}
-	}
-
-	if (onGround == true) {
-		if (KeyTrigger::CheckTrigger(KEY_INPUT_SPACE)) {
-			velocity = v0;
-			onGround = false;
-		}
-	}
-
-	if (onGround == false) {
-		if (jumpcount == Maxjumpcount) {
-			if (KeyTrigger::CheckTrigger(KEY_INPUT_SPACE)) {
-				jumpcount -= 1;
-				velocity = v0;
+		if (onGround == true) {
+			if (jumpcount < Maxjumpcount) {
+				jumpcount += 1;
 			}
 		}
-	}
-	y += velocity;
-	velocity += Gravity;
-	if (velocity >= 0) {
-		Field* field = FindGameObject<Field>();
-		int push1 = field->HitCheckDown(x + 14, y + 64); // D点の下 
-		int push2 = field->HitCheckDown(x + 50, y + 64); // A点の下 
-		int push = max(push1, push2);
-		if (push > 0) {
-			y -= push - 1;
-			velocity = 0;
-			onGround = true;
-		}
-		else {
-			onGround = false;
-		}
-	}
-	else
-	{
-		Field* field = FindGameObject<Field>();
-		int push1 = field->HitCheckUp(x + 14, y + 5); // D点の下 
-		int push2 = field->HitCheckUp(x + 50, y + 5); // A点の下 
-		int push = max(push1, push2);
-		if (push > 0) {
-			y += push;
-			velocity = 0;
-		}
-	}
-	if (moveX != 0)
-	{
-		// 移動時のアニメーション更新
-		animFrame = (animFrame + 1) % ANIM_FRAME_INTERVAL;
-		if (animFrame == 0)
+
+		if (CheckHitKey(KEY_INPUT_D))
 		{
-			animIndex = (animIndex + 1) % ANIM_FRAME_COUNT;
+			x += 3.0f; // 右に進む 
+			direction = false;
+			moveX += 2.0f;
+			Field* field = FindGameObject<Field>();
+			int push1 = field->HitCheckRight(x + 50, y + 5);
+			int push2 = field->HitCheckRight(x + 50, y + 61);
+			x -= max(push1, push2);
+			if (field->Istrap(x + 32, y + 32)) {
+				/*new Clear();*/
+			}
 		}
 
+		if (CheckHitKey(KEY_INPUT_A)) {
+			x -= 3.0f;
+			direction = true;
+			moveX -= 2.0f;
+			Field* field = FindGameObject<Field>();
+			int push1 = field->HitCheckLeft(x + 14, y + 5);
+			int push2 = field->HitCheckLeft(x + 14, y + 61);
+			x -= max(push1, push2);
+			if (field->Istrap(x + 32, y + 32)) {
+				/*new Clear();*/
+			}
+		}
+
+		if (onGround == true) {
+			if (KeyTrigger::CheckTrigger(KEY_INPUT_SPACE)) {
+				velocity = v0;
+				onGround = false;
+			}
+		}
+
+		if (onGround == false) {
+			if (jumpcount == Maxjumpcount) {
+				if (KeyTrigger::CheckTrigger(KEY_INPUT_SPACE)) {
+					jumpcount -= 1;
+					velocity = v0;
+				}
+			}
+		}
+		y += velocity;
+		velocity += Gravity;
+		if (velocity >= 0) {
+			Field* field = FindGameObject<Field>();
+			int push1 = field->HitCheckDown(x + 14, y + 64); // D点の下 
+			int push2 = field->HitCheckDown(x + 50, y + 64); // A点の下 
+			int push = max(push1, push2);
+			if (push > 0) {
+				y -= push - 1;
+				velocity = 0;
+				onGround = true;
+			}
+			else {
+				onGround = false;
+			}
+		}
+		else
+		{
+			Field* field = FindGameObject<Field>();
+			int push1 = field->HitCheckUp(x + 14, y + 5); // D点の下 
+			int push2 = field->HitCheckUp(x + 50, y + 5); // A点の下 
+			int push = max(push1, push2);
+			if (push > 0) {
+				y += push;
+				velocity = 0;
+			}
+		}
+		if (moveX != 0)
+		{
+			// 移動時のアニメーション更新
+			animFrame = (animFrame + 1) % ANIM_FRAME_INTERVAL;
+			if (animFrame == 0)
+			{
+				animIndex = (animIndex + 1) % ANIM_FRAME_COUNT;
+			}
+
+		}
+		else
+		{
+			animIndex = 0; // アニメーションのコマを最初のコマ (静止画) に固定する
+		}
 	}
-	else
-	{
-		animIndex = 0; // アニメーションのコマを最初のコマ (静止画) に固定する
-    }
+	if (state == STATE_BOOM) {
+
+		const float deltaTime = 20.0f / 60.0f;
+
+		// 1. 速度の減衰
+		velX *= (1.0f - DRAG_COEFFICIENT);
+		velY *= (1.0f - DRAG_COEFFICIENT);
+
+		// 2. 位置の更新
+		x += velX * deltaTime;
+		y += velY * deltaTime;
+
+		// 3. animeの更新
+
+
+		// 4. 画面外判定
+		if (x < -SAFE_MARGIN || x > 1920 + SAFE_MARGIN ||
+			y < -SAFE_MARGIN || y > 1080 + SAFE_MARGIN)//シーンの幅と高さを入れる
+		{
+			boomFrame = (boomFrame + 1) % BOOM_ANIM_FRAME_INTERVAL;
+			if (boomFrame == 0)
+			{
+				boomAnimIndex = (boomAnimIndex + 1) % BOOM_ANIM_FRAME_COUNT;
+			}
+			// 画面外に出たらゲームオーバー処理へ移行
+			state = STATE_GAMEOVER;
+			new GameOver(); // ここで初めてゲームオーバー画面へ移行するようにしたい
+		}
+	}
 }
 
 
 //表示するところ
 void Player::Draw()
 {
-	Field* field = FindGameObject<Field>();	
+	Field* field = FindGameObject<Field>();
 	//DrawRectGraph(x, y, 0/*CHR_SIZE * pat*/, 0/*CHR_SIZE * dir*/, 64/*CHR_SIZE*/, 64/*CHR_SIZE*/, hImage, 1);
 	//  アニメーションのコマがTextureAtlasのどこにあるか計算する
 	int xRect = (animIndex % ATLAS_WIDTH) * CHARACTER_WIDTH;
 	int yRect = (animIndex / ATLAS_WIDTH) * CHARACTER_HEIGHT;
+	int boomXRect = (boomAnimIndex % BOOM_ATLAS_WIDTH) * BOOM_CHARACTER_WIDTH;
+	int boomYRect = (boomAnimIndex / BOOM_ATLAS_WIDTH) * BOOM_CHARACTER_HEIGHT;
 
 	//  キャラクターをTextureAtlasを使って表示する
-	DrawRectGraph(x, y, xRect, yRect, CHARACTER_WIDTH, CHARACTER_HEIGHT, animImage, TRUE, direction);
+	if (state == STATE_BOOM) {
 
+		DrawRectGraph(x, y, boomXRect, boomYRect, 64, 64, BOOMImage, TRUE, direction);
+	}
+	else {
+		DrawRectGraph(x, y, xRect, yRect, CHARACTER_WIDTH, CHARACTER_HEIGHT, animImage, TRUE, direction);
+	}
+	// デバッグ用情報表示
 	DrawFormatString(0, 100, GetColor(255, 255, 255), "X::%4f", x);
 	DrawFormatString(0, 120, GetColor(255, 255, 255), "y::%4f", y);
 	DrawFormatString(0, 140, GetColor(255, 255, 255), "jumpcount::%d", jumpcount);
--- ひな形/project/Source/trap.h
+++ ひな形/project/Source/trap.h
@@ -3,7 +3,7 @@
 
 class trap : public GameObject {
 public:
-	trap(int px,int py, int i);
+	trap(int px, int py, int i);
 	~trap();
 	void Update() override;
 	void Draw() override;
