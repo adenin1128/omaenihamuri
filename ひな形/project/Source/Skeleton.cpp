2dede00b259379bc4a96ff31c044190b92b8c0b4
rilieru <sdt2595gp0003@edu.sca.ac.jp>
2025-12-03 12:12:43 +0900

    ba

--- ひな形/project/Source/Skeleton.cpp
+++ ひな形/project/Source/Skeleton.cpp
@@ -7,6 +7,8 @@ Skeleton::Skeleton(int px, int py)
 	Image = LoadGraph("data/image/bird.png");
 	ResetX = px;
 	ResetY = py;
+
+	Reset();
 }
 
 Skeleton::~Skeleton()
@@ -22,6 +24,10 @@ void Skeleton::Reset()
 
 void Skeleton::Update()
 {
+	if (CheckHitKey(KEY_INPUT_R)) {
+		DestroyMe();
+		return;
+	}
 }
 
 void Skeleton::Draw()
