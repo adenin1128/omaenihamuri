a9a3a42e7d6cbc10d04639d938b57ea8dc990801
rilieru <sdt2595gp0003@edu.sca.ac.jp>
2026-01-06 16:36:52 +0900

    ba

--- ひな形/project/Source/Mainmenu.cpp
+++ ひな形/project/Source/Mainmenu.cpp
@@ -220,7 +220,7 @@ void MainmenuScene::Update()
 			fader->FadeOut(0.1f);
 			SceneManager::ChangeScene("TITLE");
 		}
-		if (Screen::DEVELOPER_MODE == TRUE) {
+		if (Screen::DEVELOPER_MODE == TRUE || CheckHitKey(KEY_INPUT_RIGHT)) {
 			if (CheckHitKey(KEY_INPUT_ESCAPE)) {
 				fader->FadeOut(0.1f);
 				SceneManager::Exit();
