e7c2888970adf963171ec5f6ec37884fecbca064
rilieru <sdt2595gp0003@edu.sca.ac.jp>
2026-01-10 14:39:49 +0900

    ば

--- ひな形/project/Source/Timer.cpp
+++ ひな形/project/Source/Timer.cpp
@@ -1,8 +1,31 @@
 #include "Timer.h"
+#include <vector>
+#include "CsvReader.h"
 
-Timer::Timer()
+using namespace std;
+
+vector<vector<int>> Times;
+
+Timer::Timer(int stage)
 {
 	DontDestroyOnSceneChange();
+
+	char filename[60];
+	sprintf_s<60>(filename, "data/time%02d.csv", stage);
+	CsvReader* csv = new CsvReader(filename);
+	csv = new CsvReader(filename);
+	int lines = csv->GetLines();
+	lines = csv->GetLines();
+	Times.resize(lines);
+	for (int y = 0; y < lines; y++) {
+		int cols = csv->GetColumns(y);
+		Times[y].resize(cols);
+		for (int x = 0; x < cols; x++) {
+			int num = csv->GetInt(y, x);
+			Times[y][x] = num;
+		}
+	}
+
 	timer = 0.0f;
 	stopwatch = false;
 	Deathcount = 0;
@@ -35,6 +58,11 @@ void Timer::ResetTimer()
 	timer = 0.0f;
 }
 
+void Timer::AcerageTime()
+{
+
+}
+
 void Timer::deathcount()
 {
 	Deathcount++;
@@ -43,4 +71,8 @@ void Timer::deathcount()
 void Timer::ResetDeathcount()
 {
 	Deathcount = 0;
+}
+
+void Timer::AverageDeathCount()
+{
 }
